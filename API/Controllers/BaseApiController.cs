c5588ae92ff49ed5a59462a49e5b74a43330c537
saikiran37130204 <saikirankondreddy.1@gmail.com>
2025-01-08 18:21:47 -0600

    Completed activities CRUD operatioins on API side

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -6,6 +7,8 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-       
+        private IMediator _medaitor;
+
+        protected IMediator Mediator => _medaitor ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,18 @@
+using Application.Activities;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using MediatR;
 using System.Linq.Expressions;
+using Application.Core;
+using API.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -23,6 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
