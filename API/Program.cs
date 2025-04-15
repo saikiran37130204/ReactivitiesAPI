e6a6df1d205927ee97e05580c9dd84600ba19131
saikiran37130204 <saikirankondreddy.1@gmail.com>
2025-04-15 12:22:26 -0500

    setting inline styles

--- API/Program.cs
+++ API/Program.cs
@@ -36,14 +36,18 @@ app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt=>opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
 app.UseCsp(opt => opt
-  .BlockAllMixedContent()
-  .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
-  .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com","data:"))
-  .FormActions(s => s.Self())
-  .FrameAncestors(s => s.Self())
-  .ImageSources(s => s.Self().CustomSources("blob:","https://res.cloudinary.com"))
-  .ScriptSources(s => s.Self())
-  );
+    .BlockAllMixedContent()
+    .StyleSources(s => s.Self()
+        .CustomSources("https://fonts.googleapis.com")
+        .UnsafeInline()) // Only if you must have inline styles
+    .FontSources(s => s.Self()
+        .CustomSources("https://fonts.gstatic.com", "data:"))
+    .FormActions(s => s.Self())
+    .FrameAncestors(s => s.Self())
+    .ImageSources(s => s.Self()
+        .CustomSources("blob:", "https://res.cloudinary.com"))
+    .ScriptSources(s => s.Self())
+);
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
