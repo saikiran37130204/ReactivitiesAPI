e15f7bacf3e692ef05ea10c35bea656504faf625
saikiran37130204 <saikirankondreddy.1@gmail.com>
2025-04-14 23:10:11 -0500

    resolving font related issues

--- API/Program.cs
+++ API/Program.cs
@@ -37,7 +37,7 @@ app.UseXXssProtection(opt=>opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
 app.UseCsp(opt => opt
   .BlockAllMixedContent()
-  .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com/"))
+  .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
   .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com","data:"))
   .FormActions(s => s.Self())
   .FrameAncestors(s => s.Self())
